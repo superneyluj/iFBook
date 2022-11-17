2e0f6b4056e0a54f9198ef4eeed9428dfdf3c2d3
superneyluj <julien.sicot@etu.unice.fr>
2022-11-17 20:13:08 +0100

    ajout d'un menu

--- menu.cpp
+++ menu.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main(){
 
+    string choix;
 
     cout << "\t\t////////////////////////////// iFBook \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\n\n";
     cout << "Rappel des commandes :\n";
@@ -13,8 +14,16 @@ int main(){
     cout << "\t -> ADD type : lancer la procedure permettant de creer une nouvelle ressource en fonction du type specifie en parametre. \n";
     cout << "\t -> SEARCH  string : permet de rechercher dans la base de donnees les documents contenant la chaine de caracteres specifiee en parametre.\n";
     cout << "\t -> CLEAR  : reinitialise la base de donnee.\n";
-    cout << "\t -> LIST  : afficher l'intégralite du contenu de la mediatheque.\n";
-    cout << "\t -> DELETE id : cette methode permet de supprimer une ressource particuliere appartenant a la mediatheque.\n";
+    cout << "\t -> LIST  : afficher l'integralite du contenu de la mediatheque.\n";
+    cout << "\t -> DELETE id : cette methode permet de supprimer une ressource particuliere appartenant a la mediatheque.\n\n";
+    cout << "Que voulez vous faire ?\n";
+
+    std::getline(std::cin,choix);
+
+    if(choix != "BYE" and "ADD" and "LIST"){
+        cout << "Erreur dans la saisie";
+    }
+
 	return 0;
 }
 
