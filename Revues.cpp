382bfc6ec65d8bbf1e6ef486c8872af20557f595
ajorand <antoine.jorand@outlook.com>
2022-11-17 16:12:06 +0100

    modif du constructeur de Revues

--- Revues.cpp
+++ Revues.cpp
@@ -1,5 +1,5 @@
 #include <Revues.hpp>
 
-Revues::Revues(const string iEditeur, const int iNombre_article): m_editeur(iEditeur), m_nombre_article(iNombre_article), Livre(&iAuteur, &iTitre, &iMaison_edition, &iResume, &iAnnee_publication, &iNombre_pages){}
+Revues::Revues(const string &iEditeur, const int &iNombre_article, const string &iAuteur, const string &iTitre, const string &iMaison_edition, const string &iResume, const int &iAnnee_publication, const int &iNombre_pages): m_editeur(iEditeur), m_nombre_article(iNombre_article), Livre(iAuteur, iTitre, iMaison_edition, iResume, iAnnee_publication, iNombre_pages){}
 
 Revues::~Revues() {}
