306659efbd4e87c8b46da2559e5b54ea5360d215
superneyluj <julien.sicot@etu.unice.fr>
2022-11-17 16:41:52 +0100

    modif sur revue

--- Revues.hpp
+++ Revues.hpp
@@ -17,7 +17,7 @@ class Revues: public Livre
     // Méthodes
 
     public:
-        Revues(const string &iEditeur, const int &iNombre_article, const string &iAuteur, const string &iTitre, const string &iMaison_edition, const string &iResume, const int &iAnnee_publication, const int &iNombre_pages);
+        Revues(const string &iEditeur_revue, const int &iNombre_article_revue, const string &iAuteur_revue, const string &iTitre_revue, const string &iMaison_edition_revue, const string &iResume_revue, const int &iAnnee_publication_revue, const int &iNombre_pages_revue);
         
 
 };
