6df2d1b3e01e7619963642e045ba54a88b26c144
ajorand <antoine.jorand@outlook.com>
2022-11-17 17:04:03 +0100

    ajout de la classe CD

--- Revues.cpp
+++ Revues.cpp
@@ -2,3 +2,4 @@
 
 Revues::Revues(const string &iEditeur, const int &iNombre_article, const string &iAuteur_revue, const string &iTitre_revue, const string &iMaison_edition_revue, const string &iResume_revue, const int &iAnnee_publication_revue, const int &iNombre_pages_revue): m_editeur(iEditeur), m_nombre_article(iNombre_article), Livre(iAuteur_revue, iTitre_revue, iMaison_edition_revue, iResume_revue, iAnnee_publication_revue, iNombre_pages_revue){}
 
+
