0e05d071f32e8ad1c228ad95538994f862e353ec
ajorand <antoine.jorand@outlook.com>
2022-11-17 14:57:24 +0100

    ajout de revue

---
+++ Revues.hpp
@@ -0,0 +1,15 @@
+#ifndef __REVUES_HPP
+#define __REVUES_HPP
+
+#include <iostream>
+
+using namespace std;
+
+class Revues: public Livre
+{
+    string m_editeur;
+    int m_nombre_article;
+};
+
+
+#endif //__REVUES_HPP
