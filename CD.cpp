a99d99c85e5b6665c6ed655f280c49b564c17117
superneyluj <julien.sicot@etu.unice.fr>
2022-11-17 19:10:35 +0100

    correction d'indentation

--- CD.cpp
+++ CD.cpp
@@ -1,4 +1,8 @@
 #include <CD.hpp>
 
-CD::CD(const int &iDuree, const int &iNombre_de_pistes, const string &iAuteur, const string &iMaison_de_production, const string &iTitre): m_duree(iDuree), m_nombre_de_pistes(iNombre_de_pistes), m_auteur(iAuteur), m_maison_de_production(iMaison_de_production), m_titre(m_titre) {}
-
+CD::CD(const int &iDuree, const int &iNombre_de_pistes, const string &iAuteur, const string &iMaison_de_production, const string &iTitre): 
+	m_duree(iDuree), m_nombre_de_pistes(iNombre_de_pistes), 
+	m_auteur(iAuteur), 
+	m_maison_de_production(iMaison_de_production), 
+	m_titre(m_titre) 
+	{}
