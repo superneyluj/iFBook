c682c6aab34b1c8511caf4b8989a340fa661c2c2
ajorand <antoine.jorand@outlook.com>
2022-11-17 15:23:25 +0100

    ajout de revue

---
+++ Revues.cpp
@@ -0,0 +1,6 @@
+#include <Revues.hpp>
+
+Revues::Revues(const string iEditeur, const int iNombre_article): m_editeur(iEditeur), m_nombre_article(iNombre_article){}
+
+Revues::~Revue() {}
+
--- Revues.hpp
+++ Revues.hpp
@@ -1,14 +1,25 @@
 #ifndef __REVUES_HPP
 #define __REVUES_HPP
 
+#include <livre.hpp>
 #include <iostream>
 
 using namespace std;
 
 class Revues: public Livre
 {
+    // Attributs
+
     string m_editeur;
     int m_nombre_article;
+
+
+    // Méthodes
+
+    public:
+        Revues(const string iEditeur, const int iNombre_article);
+        virtual ~Revues ();
+
 };
 
 
