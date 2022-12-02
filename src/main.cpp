69b1c2c53d924a2d5d145310e00310b764e2f897
superneyluj <julien.sicot@etu.unice.fr>
2022-12-02 09:18:47 +0100

    ajout sqlite

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include <mysql.h>
-#include <mysqld_error.h>
+#include "sqlite3.h"
 
 #include "../include/CD.hpp"
 #include "../include/Dlc.hpp"
@@ -10,49 +9,31 @@
 
 using namespace std;
 
-char HOST[] = "localhost";
-char USER[] = "root";
-char PASS[] = "root";
+
 
 void mainMenu();
-void loadingDatabase();
+void creatingDatabase();
 
-int main(int argc, char** argv) {
+int main() {
 
-	loadingDatabase();
+	creatingDatabase();
 	mainMenu();
 	
 	return 0;
 }
 
-void loadingDatabase(){
-	MYSQL* obj;
+void creatingDatabase(){
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
 
-    char * livre_bdd = "CREATE TABLE Jeu_video(m_titre varchar(255), m_plateforme varchar(255), m_public_legal varchar(255), m_editeur varchar(255), m_date_de_parution int, m_resume varchar(255)); ";
-    char * cd_bdd = "CREATE TABLE CD(m_duree int, m_auteur varchar(255), m_maison_de_production varchar(255), m_titre varchar(255)); ";
-    char * dlc_bdd = "CREATE TABLE Dlc(m_jeu_original varchar(255)); ";
-    char * revues_bdd = "CREATE TABLE Revues(m_editeur varchar(255),m_nombre_article int); ";
-	
-	bool ProgramIsOpened = true;
-	
-	//connection au serveur mysql
-	
-	if(!(obj=mysql_init(0))){
-		cout << "l'objet mysql n'a pas ete cree";
-	}
-	else{
-		if(!mysql_real_connect(obj,HOST,USER,PASS,"iFbook",3306,NULL,0)){
-			cout << "erreur dans les arguments pour l'initalisation de la bdd\n";
-			cout << mysql_error(obj);
-		}
-		else{
-			mysql_query(obj,livre_bdd);
-			mysql_query(obj,cd_bdd);
-			mysql_query(obj,dlc_bdd);
-			mysql_query(obj,revues_bdd);
-			cout << "bdd chargee\n";    
-		}		
-	}	
+   rc = sqlite3_open("iFBook.db", &db);
+
+   if( rc ) {
+      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+   } else {
+      fprintf(stderr, "Opened database successfully\n");
+   }
 }
 
 void mainMenu(){
