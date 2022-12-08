12ad8cfa5b9f7a2cbf44e3a88693115cdf011d1d
superneyluj <julien.sicot@etu.unice.fr>
2022-12-08 17:11:11 +0100

    fonctions clear, delete, search OK

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@ void mainMenu();
 void creatingDatabase();
 void rechercheMenu();
 void effacementBdd();
+void effacementElement();
 int callback(void *NotUsed, int argc, char **argv, char **azColName);
 
 int main() {
@@ -23,8 +24,6 @@ int main() {
 	sqlite3 *db;
 	string query;
 	char *zErrMsg = 0;
-
-	//query = "select m_titre_livre,m_titre_jeu_video from Livre, Jeu_video;";
 
 	sqlite3_open("iFBook.db", &db);
 	sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
@@ -114,6 +113,10 @@ void mainMenu(){
 
     string choice;
 	string nom_recherche;
+	string buffer;
+	sqlite3 *db;
+	string query;
+	char *zErrMsg = 0;
 
 	system("cls");
 
@@ -124,13 +127,13 @@ void mainMenu(){
     cout << "\t -> SEARCH  string : permet de rechercher dans la base de donnees les documents contenant la chaine de caracteres specifiee en parametre.\n";
     cout << "\t -> CLEAR  : reinitialise la base de donnee.\n";
     cout << "\t -> LIST  : afficher l'integralite du contenu de la mediatheque.\n";
-    cout << "\t -> DELETE id : cette methode permet de supprimer une ressource particuliere appartenant a la mediatheque.\n\n";
+    cout << "\t -> DELETE : cette methode permet de supprimer une ressource particuliere appartenant a la mediatheque.\n\n";
     cout << "Que voulez vous faire ?\n";
 
     std::getline(std::cin,choice);
 
     if(choice=="BYE"){
-        cout << "BYE OK";
+		exit(0);
     }
     else if(choice == "ADD"){
 
@@ -143,10 +146,15 @@ void mainMenu(){
 		effacementBdd();
     }
     else if(choice == "LIST"){
-
+		sqlite3_open("iFBook.db", &db);
+		query ="SELECT m_titre_livre,m_titre_revues,m_titre_jeu_video,m_titre_cd ,m_titre_dlc FROM Livre,Revues,Jeu_video,CD,DLC;";
+		sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+		cout << "Appuyez sur Entree pour revenir au menu precedent\n";
+		std::getline(std::cin,buffer);
+		mainMenu();
     }
     else if(choice == "DELETE"){
-
+		effacementElement();
     }
 }
 
@@ -172,21 +180,40 @@ void rechercheMenu(){
 	string lowerquery;
 	sqlite3 *db;
 	char *zErrMsg = 0;
+	string buffer;
+
+	sqlite3_open("iFBook.db", &db);
 
 	system("cls");
 
 
 	cout << "Quelle est votre recherche ? :\n";
 	std::getline(std::cin,recherche);
-	query = "SELECT * FROM Livre, Jeu_video,CD, DLC,Revues WHERE m_titre_livre =" + recherche + " OR m_titre_jeu_video = " + recherche + " OR m_titre_jeu_cd = " + recherche + " OR m_titre_dlc = " + recherche + " OR m_titre_reveues = " + recherche +";" ;
-	for(char &ch : query){
-        ch = std::tolower(ch);
-    }
-	sqlite3_open("iFBook.db", &db);
+
+	query = "SELECT * FROM Livre WHERE m_titre_livre =\"" + recherche+"\";";	
 	sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+	
+	query = "SELECT * FROM Revues WHERE m_titre_revues =\"" + recherche+"\";";	
+	sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+
+	query = "SELECT * FROM Jeu_video WHERE m_titre_jeu_video =\"" + recherche+"\";";	
+	sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+
+	query = "SELECT * FROM CD WHERE m_titre_cd =\"" + recherche+"\";";	
+	sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+
+	query = "SELECT * FROM DLC WHERE m_titre_dlc =\"" + recherche+"\";";	
+	sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+
+	cout << "Recherche terminee, appuyez sur la touche entree pour revenir au menu principal";
+	std::getline(std::cin,buffer);
+	mainMenu();
+
+
 }
 
 void effacementBdd(){
+
 	sqlite3 *db;
 
 	string buffer;
@@ -197,26 +224,25 @@ void effacementBdd(){
 	cout << "Etes vous sur de vouloir effacer la base de donnes ? y/n\n";
 	std::getline(std::cin,choix);
 	if(choix == "y"){
-		
+
 		system("cls");
 		sqlite3_open("iFBook.db", &db);
 
-		query = "DELETE FROM Livre;";
+		query = "TRUNCATE FROM Livre;";
 		sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
-		query = "DELETE FROM Jeu_video;";
+		query = "TRUNCATE FROM Jeu_video;";
 		sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
-		query = "DELETE FROM CD;";
+		query = "TRUNCATE FROM CD;";
 		sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
-		query = "DELETE FROM DLC;";
+		query = "TRUNCATE FROM DLC;";
 		sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
-		query = "DELETE FROM Revues;";
+		query = "TRUNCATE FROM Revues;";
 		sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
 
 		sqlite3_close(db);
 
 		cout << "Base de donnees effacee, appuyer sur Entree touche pour revenir au menu precedent";
 		std::getline(std::cin,buffer);
-
 		mainMenu();
 	}
 	else if(choix == "n"){
@@ -227,6 +253,76 @@ void effacementBdd(){
 	else{
 		cout << "Choix invalide \n";
 		effacementBdd();
+	}
+
+}
+
+void effacementElement(){
+
+	sqlite3 *db;
+
+	char *zErrMsg = 0;
+	string buffer;
+	string choix_effacement;
+	string query;
+	string choix;
+
+	system("cls");
+
+	sqlite3_open("iFBook.db", &db);
+
+	cout << "Quel est le titre de l'element que vous voulez effacer ?\n";
+	std::getline(std::cin,choix_effacement);
+
+	query = "SELECT * FROM Livre WHERE m_titre_livre =\"" + choix_effacement+"\";";	
+	sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+	
+	query = "SELECT * FROM Revues WHERE m_titre_revues =\"" + choix_effacement+"\";";	
+	sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+
+	query = "SELECT * FROM Jeu_video WHERE m_titre_jeu_video =\"" + choix_effacement+"\";";	
+	sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+
+	query = "SELECT * FROM CD WHERE m_titre_cd =\"" + choix_effacement+"\";";	
+	sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+
+	query = "SELECT * FROM DLC WHERE m_titre_dlc =\"" + choix_effacement+"\";";	
+	sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+
+	cout << "Etes vous sur de vouloir cet element ? y/n\n";
+	std::getline(std::cin,choix);
+
+	if(choix == "y"){
+
+		query = "DELETE FROM Livre WHERE m_titre_livre =\"" + choix_effacement+"\";";	
+		sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+	
+		query = "DELETE FROM Revues WHERE m_titre_revues =\"" + choix_effacement+"\";";	
+		sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+
+		query = "DELETE FROM Jeu_video WHERE m_titre_jeu_video =\"" + choix_effacement+"\";";	
+		sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+		
+		query = "DELETE FROM CD WHERE m_titre_cd =\"" + choix_effacement+"\";";	
+		sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+
+		query = "DELETE FROM DLC WHERE m_titre_dlc =\"" + choix_effacement+"\";";	
+		sqlite3_exec(db, query.c_str(), callback, 0, &zErrMsg);
+
+		cout << "Titre efface, appuyez sur entree pour revenir au menu principal\n";
+		std::getline(std::cin,buffer);
+		mainMenu();
+
+	}
+	else if(choix == "n"){
+
+		cout << "Appuyez sur Entree pour revenir au menu precedent\n";
+		std::getline(std::cin,buffer);
+		mainMenu();
+	}
+	else{
+		cout << "Choix invalide \n";
+		effacementElement();
 	}
 
 }
